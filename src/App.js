d90a718f9d19fa67dd7c346a974981fe60cd0e07
Michael Trpak <mrtrpak3@gmail.com>
2020-06-26 15:58:16 -0400

    base setup to start working on my project section

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+  );
+};
