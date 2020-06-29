1691f5c6c16601cc15d8545d6e388531d7d56d1f
Michael Trpak <mrtrpak3@gmail.com>
2020-06-29 14:04:24 -0400

    created component to put into app

---
+++ src/PlayerLinks/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Grid, Link, IconButton, Paper } from "@material-ui/core";
+import TheatersIcon from "@material-ui/icons/Theaters";
+import GithubIcon from "@material-ui/icons/GitHub";
+
+export default function PlayerLinks() {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
