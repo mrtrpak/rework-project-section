701cf6c9248092fc4a3b775110169af3b344b4b5
Michael Trpak <mrtrpak3@gmail.com>
2020-06-29 14:11:42 -0400

    importing component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Parallax } from "react-parallax";
 
-import paraImg from "./photos/prague.jpg"
+import paraImg from "./photos/prague.jpg";
+import PlayerLinks from "./PlayerLinks";
 
 export default function App() {
 
   return (
     <Parallax bgImage={paraImg}>
-
+      <PlayerLinks />
     </Parallax>
   );
 };
