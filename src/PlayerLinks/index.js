1b8aa152c280d4454d6f2682df951a6aeb0a4c49
Michael Trpak <mrtrpak3@gmail.com>
2020-06-30 18:26:42 -0400

    importing gifs and using state with hooks

--- src/PlayerLinks/index.js
+++ src/PlayerLinks/index.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Link, IconButton, Paper } from "@material-ui/core";
 import TheatersIcon from "@material-ui/icons/Theaters";
 import GithubIcon from "@material-ui/icons/GitHub";
 
+import budgetGif from "../gifs/budget.gif";
+import businessGif from "../gifs/business.gif";
+import retroGif from "../gifs/retroGame.gif";
+import switchGif from "../gifs/switchGifs.gif";
+
 export default function PlayerLinks() {
+
+  const [projTitle, updTitle] = useState("Portfolio");
+  const [source, updSrc] = useState(switchGif);
 
   return (
     <div>
