a36178057a54a76bb5cdab086eedcb65c5e3b953
Michael Trpak <mrtrpak3@gmail.com>
2020-07-11 10:56:32 -0400

    updating titles rendered and center-aligned

--- src/PlayerLinks/index.js
+++ src/PlayerLinks/index.js
@@ -26,19 +26,19 @@ export default function PlayerLinks() {
       case "1":
         document.getElementById("projectText").innerHTML = "A free to use business card creator where a user can create cards from a selection of templates and print out multiple cards on a page.";
         document.getElementById("toolsText").innerHTML = "axios, ES6, CSS3, express, heroku, MySQL, passport, nodemon, react, sequelize";
-        updTitle("Card Creator");
+        updTitle("Business Card Creator");
         updSrc(businessGif);
         return;
       case "2":
         document.getElementById("projectText").innerHTML = "A retro style game where a user can create a character to fight a tough boss. There are four move types Normal, Special, Heal & Dodge.";
         document.getElementById("toolsText").innerHTML = "ES6, express, handlebars, , MySQL, nes.css, nodemon, sequelize";
-        updTitle("Full Stack Game");
+        updTitle("Full-Stack Retro Game");
         updSrc(retroGif);
         return;
       case "3":
         document.getElementById("projectText").innerHTML = "App that tracks spending and income to get a clear view of money expenditure. It also works offline if there is no internet connection.";
         document.getElementById("toolsText").innerHTML = "CSS3, express, JS, mongoDB, mongoose, PWA";
-        updTitle("Budget Tracker");
+        updTitle("Budget Tracker App");
         updSrc(budgetGif);
         return;
       default:
