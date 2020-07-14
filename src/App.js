1edf6f4c99c8471b0bade7ba571ce6280d53bd33
Michael Trpak <mrtrpak3@gmail.com>
2020-07-14 15:57:42 -0400

    updated syntax and removed excess

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import "./App.css";
 
 export default function App() {
 
-  const [height, setHt] = useState(650);
-  const [strength, setStr] = useState(300);
+  const [height, setHt] = useState(700);
+  const [strength, setStr] = useState(500);
 
   const updParallax = () => {
     let width = window.innerWidth;
-    console.log(width);
 
     if (width > 960) {
       setHt(700);
@@ -22,7 +21,6 @@ export default function App() {
       setHt(1000);
       setStr(300);
     };
-
   };
 
   useEffect(() => {
@@ -33,7 +31,6 @@ export default function App() {
     return () => window.removeEventListener("resize", updPage);
   }, []);
   
-
   return (
     <Parallax bgImage={paraImg} strength={strength}  >
       <div className="playLinkGrid" style={{ height: height }}>
